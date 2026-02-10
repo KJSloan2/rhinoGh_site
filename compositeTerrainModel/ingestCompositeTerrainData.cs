51cd12230b6355aed1fab37f1b51242399cd9520
Kevin Sloan <61387934+KJSloan2@users.noreply.github.com>
2026-02-10 05:59:48 -0600

    Updated variable indexing logic to account for csv structure changes. Added feature to generate bounding area 2PtRect (used for topo slicer).

--- compositeTerrainModel/ingestCompositeTerrainData.cs
+++ compositeTerrainModel/ingestCompositeTerrainData.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using Rhino.Geometry;
 using Grasshopper;
 using Grasshopper.Kernel;
@@ -42,20 +43,22 @@ public class Script_Instance : GH_ScriptInstance
 		ref object k,
 		ref object l,
 		ref object m,
-		ref object n)
+		ref object n,
+		ref object o)
   {
     string fileName = x as string;
+    var headerIndex = new Dictionary<string, int>();
 
     double scaleX = 1.0, scaleY = 1.0;
 
-    if (zz is IList list && list.Count >= 2) // covers double[], float[], List<double>, List<float>, object[]
+    if (zz is IList list && list.Count >= 2)                    // covers double[], float[], List<double>, List<float>, object[]
     {
         scaleX = Convert.ToDouble(list[0], INV);
         scaleY = Convert.ToDouble(list[1], INV);
     };
 
     double adjX = 1.0, adjY = 1.0;
-    if (za is IList zaList && zaList.Count >= 2) // covers double[], float[], List<double>, List<float>, object[]
+    if (za is IList zaList && zaList.Count >= 2)                    // covers double[], float[], List<double>, List<float>, object[]
     {
         adjX = Convert.ToDouble(zaList[0], INV);
         adjY = Convert.ToDouble(zaList[1], INV);
@@ -64,28 +67,36 @@ public class Script_Instance : GH_ScriptInstance
     bool run = (bool) y;
 
     var points = new List<Point3d>();
-    var lstf_serc = new List<float>();
+    var pointsTransformed = new List<Point3d>();
+    var ddPoints = new List<Point3d>();
+    var lstf_serc = new List<double>();
     var lstf_flag = new List<string>();
-    var elv_rel = new List<float>();
-    var lstf_mean = new List<float>();
-    var ndvi_mean = new List<float>();
+    var elv_rel = new List<double>();
+    var lstf_mean = new List<double>();
+    var ndvi_mean = new List<double>();
     var ndvi_flag = new List<string>();
-    var ndmi_mean = new List<float>();
+    var ndmi_mean = new List<double>();
     var idx_row = new List<int>();
     var idx_col = new List<int>();
     var dd_geoid = new List<int>();
+    var mc_geoid = new List<int>();
 
-    var dom_dir_elv = new List<float>();
+    var poolCoordsX = new List<double>();
+    var poolCoordsY = new List<double>();
+
+    var dom_dir_elv = new List<double>();
     var dom_dir = new List<string>();
+
+    var bbRect = new Rectangle3d();
 
     if (!run || string.IsNullOrEmpty(fileName))
     { a=points; b=lstf_serc; c=lstf_flag; 
     d=elv_rel; e=lstf_mean; f=ndvi_mean; 
     g=ndvi_flag; h=ndmi_mean; i=idx_row; 
     j=idx_col; k=dom_dir; l=dom_dir_elv;
-    m=dd_geoid; return; }
+    m=dd_geoid; n=mc_geoid; o=bbRect; return; }
     
-    string dirPath = @"DIRECTORY_PATH_HERE"; // <-- UPDATE THIS TO YOUR DIRECTORY
+    string dirPath = @"C:\Users\Kjslo\Documents\local_dev\dll_p\output\aoi_filtered";
 
     string fullPath = Path.Combine(dirPath, fileName);
 
@@ -96,7 +107,7 @@ public class Script_Instance : GH_ScriptInstance
       d=elv_rel; e=lstf_mean; f=ndvi_mean; 
       g=ndvi_flag; h=ndmi_mean; i=idx_row; 
       j=idx_col; k=dom_dir; l=dom_dir_elv; 
-      m=dd_geoid; return;
+      m=dd_geoid; n=mc_geoid; o=bbRect; return;
     }
 
     // Toggle if your altitude column is meters
@@ -112,9 +123,36 @@ public class Script_Instance : GH_ScriptInstance
 
       using (var reader = new StreamReader(fullPath))
       {
-        // Skip header
-        var headerLine = reader.ReadLine();
+        string headerLine = reader.ReadLine();
+        //var headerLine = reader.ReadLine();
+        if (headerLine == null)
+            throw new Exception("CSV has no header row");
+        
+        var headers = headerLine.Split(',');
 
+        for (int itterI = 0; itterI < headers.Length; itterI++)
+        {
+            string hi = headers[itterI].Trim();
+            if (!headerIndex.ContainsKey(hi))
+                headerIndex.Add(hi, itterI);
+        }
+        ////////////////////////////////////////
+        int geoidHIdx = headerIndex["geoid"];
+        int latHIdx = headerIndex["lat"];
+        int lonHIdx = headerIndex["lon"];
+        int lstfHIdx = headerIndex["lstf"];
+        int lstfSercHIdx = headerIndex["lstf_serc"];
+        int ndviHIdx = headerIndex["ndvi"];
+        int ndmiHIdx = headerIndex["ndmi"];
+        int elvRelHIdx = headerIndex["elv_rel"];
+        int rowIdxHIdx = headerIndex["idx_row"];
+        int colIdxHIdx = headerIndex["idx_col"];
+        int domDirHIdx = headerIndex["dom_dir"];
+        int ddGeoidHIdx = headerIndex["dd_geoid"];
+        int ddElvHIdx = headerIndex["dom_dir_elv"];
+        int ddPtbLatHIdx = headerIndex["dom_ptb_lat"];
+        int ddPtbLonHIdx = headerIndex["dom_ptb_lon"];
+        ////////////////////////////////////////
         string line;
         while ((line = reader.ReadLine()) != null)
         {
@@ -122,20 +160,23 @@ public class Script_Instance : GH_ScriptInstance
           var tokens = line.Split(',');
           if (tokens.Length < 18) continue;
 
-          if (!double.TryParse(tokens[3], NumberStyles.Float, INV, out double lstfVal))  continue;
-          if (!double.TryParse(tokens[4], NumberStyles.Float, INV, out double lstfSerc)) continue;
+          if (!double.TryParse(tokens[lstfHIdx], NumberStyles.Float, INV, out double lstfVal))  continue;
+          if (!double.TryParse(tokens[lstfSercHIdx], NumberStyles.Float, INV, out double lstfSerc)) continue;
           //string lstfFlag = tokens[6];
 
-          if (!double.TryParse(tokens[6], NumberStyles.Float, INV, out double ndviVal)) continue;
+          if (!double.TryParse(tokens[ndviHIdx], NumberStyles.Float, INV, out double ndviVal)) continue;
           //string ndviFlag = tokens[10];
 
-          if (!double.TryParse(tokens[9], NumberStyles.Float, INV, out double ndmiVal)) continue;
+          if (!double.TryParse(tokens[ndmiHIdx], NumberStyles.Float, INV, out double ndmiVal)) continue;
 
-          if (!double.TryParse(tokens[1], NumberStyles.Float, INV, out double lat)) continue;
-          if (!double.TryParse(tokens[2], NumberStyles.Float, INV, out double lon)) continue;
+          if (!double.TryParse(tokens[latHIdx], NumberStyles.Float, INV, out double lat)) continue;
+          if (!double.TryParse(tokens[lonHIdx], NumberStyles.Float, INV, out double lon)) continue;
+
+          if (!double.TryParse(tokens[ddPtbLatHIdx], NumberStyles.Float, INV, out double ddPtbLat)) continue;
+          if (!double.TryParse(tokens[ddPtbLonHIdx], NumberStyles.Float, INV, out double ddPtbLon)) continue;
 
           // elv_rel column (tokens[12]); keep in feet (convert if meters)
-          if (!double.TryParse(tokens[12], NumberStyles.Float,   INV, out double elvRel)) continue;
+          if (!double.TryParse(tokens[elvRelHIdx], NumberStyles.Float,   INV, out double elvRel)) continue;
           
           double altFeet = ALT_IS_METERS ? elvRel * FT_PER_M : elvRel;
           //double altFeet = elvRel;
@@ -154,13 +195,16 @@ public class Script_Instance : GH_ScriptInstance
           ndvi_mean.Add((float)ndviVal);
           ndmi_mean.Add((float)ndmiVal);
 
-          if (!int.TryParse(tokens[14], NumberStyles.Integer, INV, out int rIndex)) rIndex = 0;
-          if (!int.TryParse(tokens[15], NumberStyles.Integer, INV, out int cIndex)) cIndex = 0;
+          if (!int.TryParse(tokens[rowIdxHIdx], NumberStyles.Integer, INV, out int rIndex)) rIndex = 0;
+          if (!int.TryParse(tokens[colIdxHIdx], NumberStyles.Integer, INV, out int cIndex)) cIndex = 0;
           idx_row.Add(rIndex);
           idx_col.Add(cIndex);
     
-          if (!int.TryParse(tokens[40], NumberStyles.Integer, INV, out int ddGeoid)) ddGeoid = 0;
+          if (!int.TryParse(tokens[ddGeoidHIdx], NumberStyles.Integer, INV, out int ddGeoid)) ddGeoid = 0;
           dd_geoid.Add(ddGeoid);
+
+          if (!int.TryParse(tokens[geoidHIdx], NumberStyles.Integer, INV, out int geoid)) geoid = 0;
+          mc_geoid.Add(geoid);
           //////////////////////////////////////////////////////////////////////////////////////
           // Debug logging
           /*if (points.Count < 5)
@@ -170,13 +214,13 @@ public class Script_Instance : GH_ScriptInstance
           }*/
           //////////////////////////////////////////////////////////////////////////////////////
           //string domDir = tokens.Length > 38 ? tokens[38] : "";
-          string domDir = tokens[38];
+          string domDir = tokens[domDirHIdx];
           dom_dir.Add(domDir);
 
           float domDirElvVal = 0.0f;
-          if (tokens.Length > 39)
+          if (tokens.Length > 51)
           {
-            float.TryParse(tokens[39], NumberStyles.Float, INV, out domDirElvVal);
+            float.TryParse(tokens[ddElvHIdx], NumberStyles.Float, INV, out domDirElvVal);
           }
           dom_dir_elv.Add(domDirElvVal);
         }
@@ -190,16 +234,40 @@ public class Script_Instance : GH_ScriptInstance
         FeetPerDegree(refLat, out double feetPerDegLon, out double feetPerDegLat);
 
         // Non-uniform scale about WorldXY (Z left as-is)
-        // Transform scaleXY = Transform.Scale(Plane.WorldXY, feetPerDegLon, feetPerDegLat, 1.0);
+        //Transform scaleXY = Transform.Scale(Plane.WorldXY, feetPerDegLon, feetPerDegLat, 1.0);
         Transform scaleXY = Transform.Scale(Plane.WorldXY, scaleX, scaleY, 1.0);
-
+        Transform moveXY  = Transform.Translation(-adjX, -adjY, 0);
+         
         foreach (var pt in rawPoints)
         {
-          var p = pt;        // struct copy
-          p.Transform(scaleXY);
-          points.Add(p);
+            var p = pt;
+            p.Transform(scaleXY);
+            p.Transform(moveXY);
+
+            pointsTransformed.Add(p);
+
+            poolCoordsX.Add(p.X);
+            poolCoordsY.Add(p.Y);
         }
       }
+
+      double minCoordsX = poolCoordsX.Min();
+      double minCoordsY = poolCoordsY.Min();
+
+      double maxCoordsX = poolCoordsX.Max();
+      double maxCoordsY = poolCoordsY.Max();
+
+      var bbPtNe = new Point3d(maxCoordsX, maxCoordsY, 0);
+      var bbPtSw = new Point3d(minCoordsX, minCoordsY, 0);
+
+      Plane plane = Plane.WorldXY;
+
+      Interval xi = new Interval(minCoordsX, maxCoordsX);
+      Interval yi = new Interval(minCoordsY, maxCoordsY);
+
+      Rectangle3d bbRect3d = new Rectangle3d(plane, xi, yi);
+      bbRect = bbRect3d;
+
     }
     catch (Exception ex)
     {
@@ -207,7 +275,7 @@ public class Script_Instance : GH_ScriptInstance
     }
 
     // Outputs
-    a = points;
+    a = pointsTransformed;
     b = lstf_serc;
     c = lstf_flag;
     d = elv_rel;
@@ -220,5 +288,7 @@ public class Script_Instance : GH_ScriptInstance
     k = dom_dir;
     l = dom_dir_elv;
     m = dd_geoid;
+    n = mc_geoid;
+    o = bbRect;
   }
 }
